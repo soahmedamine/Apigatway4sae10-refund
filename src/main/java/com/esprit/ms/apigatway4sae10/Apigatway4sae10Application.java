9b091c81040df8b1e97f49190e11f9469aaf0231
ahmed amine soualhi <soualhi.ahmedamine@esprit.tn>
2025-04-05 16:09:29 +0100

    CrossOrigin for angular project to work as it should

--- src/main/java/com/esprit/ms/apigatway4sae10/Apigatway4sae10Application.java
+++ src/main/java/com/esprit/ms/apigatway4sae10/Apigatway4sae10Application.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @SpringBootApplication
 public class Apigatway4sae10Application {
 
@@ -30,5 +31,21 @@ public class Apigatway4sae10Application {
                         r -> r.path("/refund/**").uri("lb://refund-web-distibue")
                
                 ).build();
+    }
+
+
+
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200"); // Frontend origin
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
     }
 }
